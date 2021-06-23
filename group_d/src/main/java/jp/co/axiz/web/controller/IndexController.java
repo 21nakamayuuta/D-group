a6428c9024fe6ceebf56f1c7edf968ef8f9c278a
Nagayama <21nagayama_ruka@axiz.co.jp>
2021-06-23 19:02:42 +0900

    カテゴリ検索機能の反映 top画面
    反映するにあたって、top.jspとIndexControllerを変更しました。
    IndexControllerは、ModelAttoributeの追加と表示の処理を追加しています。

--- group_d/src/main/java/jp/co/axiz/web/controller/IndexController.java
+++ group_d/src/main/java/jp/co/axiz/web/controller/IndexController.java
@@ -15,7 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import jp.co.axiz.web.controller.form.LoginForm;
 import jp.co.axiz.web.controller.form.SearchForm;
 import jp.co.axiz.web.controller.form.SignUpForm;
+import jp.co.axiz.web.entity.Category;
 import jp.co.axiz.web.entity.Recipe;
+import jp.co.axiz.web.service.CategoryService;
 import jp.co.axiz.web.service.RecipeService;
 
 @Controller
@@ -23,13 +25,18 @@ public class IndexController {
 
 	@Autowired
 	private RecipeService recipeService;
+
+	@Autowired
+	CategoryService categoryService;
+
 	@Autowired
 	HttpSession session;
 
 	@RequestMapping("/top" )
 
-		public String top(@ModelAttribute("loginForm") LoginForm loginForm,
-				@ModelAttribute("RecipeSearch") SearchForm searchForm,@ModelAttribute("sign") SignUpForm signForm,Model model) {
+	public String top(@ModelAttribute("loginForm") LoginForm loginForm,
+			@ModelAttribute("RecipeSearch") SearchForm searchForm,@ModelAttribute("sign") SignUpForm signForm,
+			@ModelAttribute("categorySearch") SearchForm categorySearchForm, Model model) {
 
 		//新着レシピ
 		List<Recipe> recipeList = recipeService.newRecipe();
@@ -38,6 +45,10 @@ public class IndexController {
 		//ランキング
 		List<Recipe> rankingList = recipeService.ranking();
 		model.addAttribute("rankingList",rankingList);
+
+		//カテゴリの表示
+		List<Category> categoryList = categoryService.searchCategory();
+		model.addAttribute("categoryList", categoryList);
 
 		session.setAttribute("login",true);
 		return "top";
