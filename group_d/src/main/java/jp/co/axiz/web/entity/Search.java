cd609c1863f6e32656900732951fd3ab1787eadf
maeshiro ryuki <21maeshiro_ryuuki@axiz.co.jp>
2021-06-25 19:22:57 +0900

    ユーザページを完成させました。

--- group_d/src/main/java/jp/co/axiz/web/entity/Search.java
+++ group_d/src/main/java/jp/co/axiz/web/entity/Search.java
@@ -1,43 +1,67 @@
 package jp.co.axiz.web.entity;
 
 public class Search {
+	private String userId;
+	private String userName;
 	private Integer recipeId;
 	private String recipeTitle;
 	private String completeImage;
 	private Integer goodCount;
 
-	public Search(){
-
+	public Search() {
 	}
 
-	public Search(Integer recipeId, String recipeTitle, String completeImage, Integer goodCount){
+	public Search(Integer recipeId, String recipeTitle, String completeImage, Integer goodCount) {
 		this.recipeId = recipeId;
 		this.recipeTitle = recipeTitle;
 		this.completeImage = completeImage;
 		this.goodCount = goodCount;
 	}
 
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
 	public Integer getRecipeId() {
 		return recipeId;
 	}
+
 	public void setRecipeId(Integer recipeId) {
 		this.recipeId = recipeId;
 	}
+
 	public String getRecipeTitle() {
 		return recipeTitle;
 	}
+
 	public void setRecipeTitle(String recipeTitle) {
 		this.recipeTitle = recipeTitle;
 	}
+
 	public String getCompleteImage() {
 		return completeImage;
 	}
+
 	public void setCompleteImage(String completeImage) {
 		this.completeImage = completeImage;
 	}
+
 	public Integer getGoodCount() {
 		return goodCount;
 	}
+
 	public void setGoodCount(Integer goodCount) {
 		this.goodCount = goodCount;
 	}
--- group_d/src/main/webapp/WEB-INF/views/recipe.jsp
+++ group_d/src/main/webapp/WEB-INF/views/recipe.jsp
@@ -171,7 +171,9 @@
             ></span
             > ${fn:escapeXml(recipeInfo.recipeTitle)}
           </h3>
-          <a href="./user.html" class="post-user">
+
+
+          <a href="/user?userID=${fn:escapeXml(recipeInfo.userId)}" class="post-user">
             <span
               class="iconify"
               data-inline="false"
@@ -179,8 +181,9 @@
             ></span>
             ${fn:escapeXml(recipeInfo.userName)}
           </a>
-          <div class="cuisine-img">
 
+
+          <div class="cuisine-img">
 <!--               src="https://dummyimage.com/600x400/dee0ff/edeeff.png" -->
             <img
 			  src= "${fn:escapeXml(recipeInfo.completeImage)}"
