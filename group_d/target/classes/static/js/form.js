23ed9c0b67cb236e0716b5a9048c2ad9f647c79b
nakama <21nakama_yuuta@axiz.co.jp>
2021-06-29 14:19:31 +0900

    マイページの更新しました。

--- group_d/target/classes/static/js/form.js
+++ group_d/target/classes/static/js/form.js
@@ -6,35 +6,35 @@ let myPass = $(".edit-pass #pass");
 let editBtnOfPassForMyPage = $(".edit-pass .edit");
 let saveBtnOfPassForMyPage = $(".edit-pass .save");
 
-editBtnOfNameForMyPage.click(() => {
-    if (myName.prop("disabled")) {
-        myName.prop("disabled", false);
-        editBtnOfNameForMyPage.toggleClass("display-none");
-        saveBtnOfNameForMyPage.toggleClass("display-none");
-    }
-});
-saveBtnOfNameForMyPage.click(() => {
-    if (!myName.prop("disabled")) {
-        myName.prop("disabled", true);
-        editBtnOfNameForMyPage.toggleClass("display-none");
-        saveBtnOfNameForMyPage.toggleClass("display-none");
-    }
-});
+// editBtnOfNameForMyPage.click(() => {
+//     if (myName.prop("disabled")) {
+//         myName.prop("disabled", false);
+//         editBtnOfNameForMyPage.toggleClass("display-none");
+//         saveBtnOfNameForMyPage.toggleClass("display-none");
+//     }
+// });
+// saveBtnOfNameForMyPage.click(() => {
+//     if (!myName.prop("disabled")) {
+//         myName.prop("disabled", true);
+//         editBtnOfNameForMyPage.toggleClass("display-none");
+//         saveBtnOfNameForMyPage.toggleClass("display-none");
+//     }
+// });
 
-editBtnOfPassForMyPage.click(() => {
-    if (myPass.prop("disabled")) {
-        myPass.prop("disabled", false);
-        editBtnOfPassForMyPage.toggleClass("display-none");
-        saveBtnOfPassForMyPage.toggleClass("display-none");
-    }
-});
-saveBtnOfPassForMyPage.click(() => {
-    if (!myPass.prop("disabled")) {
-        myPass.prop("disabled", true);
-        editBtnOfPassForMyPage.toggleClass("display-none");
-        saveBtnOfPassForMyPage.toggleClass("display-none");
-    }
-});
+// editBtnOfPassForMyPage.click(() => {
+//     if (myPass.prop("disabled")) {
+//         myPass.prop("disabled", false);
+//         editBtnOfPassForMyPage.toggleClass("display-none");
+//         saveBtnOfPassForMyPage.toggleClass("display-none");
+//     }
+// });
+// saveBtnOfPassForMyPage.click(() => {
+//     if (!myPass.prop("disabled")) {
+//         myPass.prop("disabled", true);
+//         editBtnOfPassForMyPage.toggleClass("display-none");
+//         saveBtnOfPassForMyPage.toggleClass("display-none");
+//     }
+// });
 
 // admin
 let editCategoryBtn = $(".categories .edit");
